12a13fb27bd8e44419885916d19be0870bb20b25
Steven Ayers <steveay99@gmail.com>
2017-08-03 12:59:15 +0100

    Fixing shell scripts

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
 
 srvs=("rabbitmq redis server uchiwa")
 for i in $srvs; do
 	docker rm $i -f
-	docker build -t sensu_$i ./$i
+	docker build -t sensu_$i ./build/$i
 done
 
 #docker-compose up
--- setup_ssl.sh
+++ setup_ssl.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 opt=$1
 
 usage () {
