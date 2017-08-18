46f7e7f65e00be7eb8b38aa5a31e1fd05dc219f0
Steven Ayers <steveay99@gmail.com>
2017-08-18 11:19:12 +0100

    Fix ssl script

--- setup_ssl.sh
+++ setup_ssl.sh
@@ -15,13 +15,14 @@ generate () {
     clean
     mkdir -p volumes/ssl/client volumes/ssl/server
     cd volumes/ssl
+    cp sensu_ssl_tool.tar volumes/ssl/
     tar -xvf sensu_ssl_tool.tar
     cd sensu_ssl_tool
     bash ./ssl_certs.sh generate
     cp ./sensu_ca/cacert.pem ./server/key.pem ./server/cert.pem ../server/
     cp ./client/key.pem ./client/cert.pem ../client/
     cd ..
-    rm -rf sensu_ssl_tool
+    rm -rf sensu_ssl_tool sensu_ssl_tool.tar
     cd ../..
 }
 
